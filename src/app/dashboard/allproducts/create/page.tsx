ab0b248f39e4dea0d3ec4c42459ca386fcedf381
Bipesh <bipesh@gmail.com>
2025-01-31 14:09:02 +0545

    commit 13

--- src/app/dashboard/allproducts/create/page.tsx
+++ src/app/dashboard/allproducts/create/page.tsx
@@ -3,6 +3,8 @@ import { categories } from "@/db/schema";
 import CreateProductForm from "@/components/Createproductform";
 import { Suspense } from "react";
 
+export const runtime = "nodejs";
+
 export default async function CreateProductPage() {
     const {db}= await dbconfig()
   const allCategories = await db.select().from(categories);
